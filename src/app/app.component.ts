4bfd565db8823ccf31e4523a2f2756c01e4fd743
Lorent Lempereur <lorent@commuty.net>
2020-04-21 08:27:03 +0200

    Issue 263 - Reproduce warning on source maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LatLngBounds, LayerGroup, layerGroup, tileLayer, latLng } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-leaflet-sourcemaps-warnings';
+  public options: any;
+  public fitBounds: LatLngBounds;
+  private firstLayer: LayerGroup;
+  private secondLayer: LayerGroup;
+
+    constructor() {
+      this.firstLayer = layerGroup();
+      this.secondLayer = layerGroup();
+      this.resetMap();
+      this.options = {
+        layers: [
+          tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18
+          }),
+          this.firstLayer,
+          this.secondLayer
+        ],
+        zoom: 8,
+        center: latLng(0, 0),
+        fitBounds: this.fitBounds
+      };
+    }
+
+    private resetMap() {
+      this.firstLayer.clearLayers();
+      this.secondLayer.clearLayers();
+      this.fitBounds = latLng(0, 0).toBounds(100);
+    }
 }
